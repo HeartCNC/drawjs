fd57623be617d92894c86dc290d3082a86cc33be
heartcnc <heartcnc@163.com>
2021-12-22 11:56:13 +0800

    fix: typings

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,7 @@
-// import {
-//   Painter,
-//   createPainter
-// } from './Paint/index'
-// import {
-//   Circle
-// } from './DisplayObject/Circle'
-// import {
-//   Rectangle
-// } from './DisplayObject/Rectangle'
-import { Text } from './DisplayObject/Text'
-import { Texture } from './DisplayObject/Texture'
 import { Draw } from "./Draw"
 import * as utils from './utils/index'
 
 export {
-  utils,
-  //   createPainter,
-  //   Circle,
-  //   Painter,
-  //   Rectangle,
-  Texture,
-  Text
+  utils
 }
 export default Draw
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,4 @@
-import { Text } from './DisplayObject/Text';
-import { Texture } from './DisplayObject/Texture';
 import { Draw } from "./Draw";
 import * as utils from './utils/index';
-export { utils, Texture, Text };
+export { utils };
 export default Draw;
